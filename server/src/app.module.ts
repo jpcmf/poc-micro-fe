80ee8c8934732d682c71ba76ceb29261ecdeed1a
João Paulo Fricks <jpfricks@gmail.com>
2021-11-11 21:48:52 -0300

    feat: Configure cart module server ⚙️

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 
-import { AuthorizedModule } from './modules/authorized/authorized.module';
+import { CartModule } from './modules/cart/cart.module';
 import { ProductsModule } from './modules/products/products.module';
 import { AppController } from './app.controller';
 import { AuthModule } from './auth/auth.module';
@@ -15,7 +15,7 @@ import { UsersService } from './users/users.service';
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
-    AuthorizedModule,
+    CartModule,
     ProductsModule,
     AuthModule,
   ],
